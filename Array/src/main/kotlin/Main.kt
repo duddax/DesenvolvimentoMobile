11a55e57e53e8936ec285c0c35787139569b7c77
FIC <fic@info.local>
2023-05-27 09:57:44 -0300

    Exercícios de lógica

---
+++ Array/src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+import java.util.*
+fun main(args: Array<String>) {
+// Escreva um programa onde informamos 4 idades distintas e depois exibir a maior e a menor idade informada
+
+//    var idades = IntArray (4);
+//
+//        idades [0] = 60;
+//        idades [1] = 30;
+//        idades [2] = 35;
+//        idades [3] = 70;
+//
+//    var maiorIdade = 0;
+//    var menorIdade = 0;
+//
+//    for (indice in 0 until 4)
+//    {
+//        if (indice == 0) // usada como comparação p/ 1ª analise
+//        {
+//            maiorIdade = idades[indice] // Array sempre trabalha com []
+//            menorIdade = idades[indice]
+//        } else {
+//            if (idades [indice] > maiorIdade) // Verificação se é maior que a idade já definida.
+//            {
+//                maiorIdade = idades[indice];
+//            }
+//            if (idades[indice] < menorIdade) // Verificação se é menor que a idade já definida.
+//            {
+//                menorIdade = idades[indice];
+//            }
+//        }
+//    }
+//    println(maiorIdade); // Exibição
+//    println(menorIdade);
+
+
+// Crie um programa que receba 5 salários de funcionários e aplique um aumento de 10% sobre o valor
+
+//    var salarios = Array<Double>(5) { 0.00 }
+//
+//    salarios[0] = 1320.00
+//    salarios[1] = 2500.00
+//    salarios[2] = 3980.50
+//    salarios[3] = 4700.00
+//    salarios[4] = 1200.00
+//
+//    for (indice in 0 until 5) {
+//        var acrescimo = (salarios[indice] *10) / 100 // multiplicar pra depois dividir para ter os 10%
+//            salarios [indice] = salarios[indice] + acrescimo; // para ter o valor do sálario final
+//
+//        println(salarios[indice]); // exibição na tela
+//    }
+
+
+
+}
